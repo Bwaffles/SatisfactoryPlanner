59ee3c8e37dd23c81f3c4059d4679db39a399c05
Bwaffles <thanaannis@gmail.com>
2022-06-01 10:15:53 -0400

    Updated registration form to have some validation and extracted an Input component

--- src/UI/SatisfactoryPlanner.UI/src/Registration.tsx
+++ src/UI/SatisfactoryPlanner.UI/src/Registration.tsx
@@ -1,14 +1,13 @@
-import { FormEvent, ChangeEvent } from "react";
 import * as React from "react"
 import HttpClient from "./HttpClient";
+import Input from "./Components/Input";
+import { Formik } from "formik";
+import { object, string } from "yup";
 
 type RegistrationProps = {
 };
 
 type RegistrationState = {
-    login: string;
-    email: string;
-    password: string;
     errorMessage: string;
     successMessage: string;
 };
@@ -19,116 +18,140 @@ export class Registration extends React.Component<RegistrationProps, Registratio
         super(props);
 
         this.state = {
-            login: "",
-            email: "",
-            password: "",
             errorMessage: "",
             successMessage: ""
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleUserRegistrationSuccess = this.handleUserRegistrationSuccess.bind(this);
-        this.handleLoginChange = this.handleLoginChange.bind(this);
-        this.handleEmailChange = this.handleEmailChange.bind(this);
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
     }
 
-    handleSubmit(event: FormEvent<HTMLFormElement>) {
-        const { login, email, password } = this.state;
+    handleSubmit(values: { email: string, login: string, password: string }) {
+
         var confirmLink = `${window.location.origin}/registration-confirm/`;
         const request = {
-            login: login,
-            email: email,
-            password: password,
+            login: values.login,
+            email: values.email,
+            password: values.password,
             confirmLink: confirmLink
         }
 
         HttpClient.post<any>("userAccess/userRegistrations", JSON.stringify(request))
             .then(() => this.handleUserRegistrationSuccess())
             .catch(() => {
-                this.setState({ errorMessage: 'Error during registration' });
+                this.setState({ errorMessage: "Error during registration" });
             });
-
-        event.preventDefault();
     }
 
     handleUserRegistrationSuccess() {
-        this.setState({ successMessage: 'Registration success' });
-    }
-
-    handleLoginChange(event: ChangeEvent<HTMLInputElement>) {
-        this.setState({ login: event.target.value });
-    }
-
-    handleEmailChange(event: ChangeEvent<HTMLInputElement>) {
-        this.setState({ email: event.target.value });
-    }
-
-    handlePasswordChange(event: ChangeEvent<HTMLInputElement>) {
-        this.setState({ password: event.target.value });
+        this.setState({ successMessage: "Registration success" });
     }
 
     render() {
-        const { login, email, password, successMessage, errorMessage } = this.state;
+        const { successMessage, errorMessage } = this.state;
 
         return (
             <main className="mx-auto flex min-h-screen w-full items-center justify-center bg-gray-900 text-white font-mono">
                 <section className="flex w-[30rem] flex-col space-y-10">
                     <div className="text-center text-4xl font-medium">Satisfactory Planner</div>
 
-                    {successMessage !== null &&
+                    {successMessage !== "" &&
                         <div>{successMessage}</div>
                     }
 
-                    {errorMessage !== null &&
+                    {errorMessage !== "" &&
                         <div>{errorMessage}</div>
                     }
+
                     {successMessage === "" &&
+                        <Formik
+                            initialValues={{ email: "", login: "", password: "" }}
+                            validationSchema={object({
+                                login: string()
+                                    .max(100, "Must be 100 characters or less.")
+                                    .required("Required"),
+                                email: string()
+                                    .email("Invalid email address.")
+                                    .max(254, "Must be 254 characters or less.")
+                                    .required("Required"),
+                                password: string()
+                                    .max(255, "Must be 255 characters or less.")
+                                    .required("Required"),
 
-                        <form onSubmit={(e: FormEvent<HTMLFormElement>) => this.handleSubmit(e)}>
+                            })}
+                            onSubmit={(values) => {
+                                this.handleSubmit(values);
+                            }}
+                        >
+                            {(formik: any) => (
+                                <form onSubmit={formik.handleSubmit}>
+                                    <div className="mb-6">
+                                        <label htmlFor="email" className="block mb-2 text-gray-300">
+                                            Email <span className="text-red-700">*</span>
+                                        </label>
+                                        <Input
+                                            name="email"
+                                            type="text"
+                                            {...formik.getFieldProps("email")}
+                                        />
+                                        {formik.touched.email && formik.errors.email ? (
 
-                            <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-sky-500 mb-10">
-                                <input
-                                    type="text"
-                                    placeholder="Username"
-                                    className="w-full border-none bg-transparent outline-none focus:outline-none"
-                                    value={login}
-                                    onChange={(e: ChangeEvent<HTMLInputElement>) => this.handleLoginChange(e)}
-                                />
-                            </div>
-                            <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-sky-500 mb-10">
-                                <input
-                                    type="text"
-                                    placeholder="Email"
-                                    className="w-full border-none bg-transparent outline-none focus:outline-none"
-                                    value={email}
-                                    onChange={(e: ChangeEvent<HTMLInputElement>) => this.handleEmailChange(e)}
-                                />
-                            </div>
-                            <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-sky-500 mb-10">
-                                <input
-                                    type="password"
-                                    autoComplete="password"
-                                    placeholder="Password"
-                                    className="w-full border-none bg-transparent outline-none focus:outline-none"
-                                    value={password}
-                                    onChange={(e: ChangeEvent<HTMLInputElement>) => this.handlePasswordChange(e)}
-                                />
-                            </div>
-                            <button
-                                className="w-full py-3 text-xl font-bold uppercase rounded bg-sky-800 duration-300 hover:bg-sky-900 mb-5"
-                                type="submit"
-                            >
-                                Register
-                            </button>
-                            <br />
-                            <a
-                                href="/login"
-                                className="transform text-center text-lg font-bold text-sky-600 duration-300 hover:text-sky-800 mb-10"
-                            >
-                                Back to login
-                            </a>
-                        </form>
+                                            <div className="text-red-700 mt-2">{formik.errors.email}</div>
+
+                                        ) : null}
+                                    </div>
+
+                                    <div className="mb-6">
+                                        <label htmlFor="login" className="block mb-2 text-gray-300">
+                                            Username <span className="text-red-700">*</span>
+                                        </label>
+                                        <Input
+                                            name="login"
+                                            type="text"
+                                            {...formik.getFieldProps("login")}
+                                        />
+                                        {formik.touched.login && formik.errors.login ? (
+
+                                            <div className="text-red-700 mt-2">{formik.errors.login}</div>
+
+                                        ) : null}
+                                    </div>
+
+                                    <div className="mb-6">
+                                        <label htmlFor="password" className="block mb-2 text-gray-300">
+                                            Password <span className="text-red-700">*</span>
+                                        </label>
+                                        <Input
+                                            name="password"
+                                            autoComplete="new-password"
+                                            type="password"
+                                            {...formik.getFieldProps("password")}
+                                        />
+                                        {formik.touched.password && formik.errors.password ? (
+
+                                            <div className="text-red-700 mt-2">{formik.errors.password}</div>
+
+                                        ) : null}
+                                    </div>
+
+                                    <button
+                                        className="w-full py-3 text-xl font-bold uppercase rounded bg-sky-800 duration-300 hover:bg-sky-900 mb-5"
+                                        type="submit"
+                                    >
+                                        Register
+                                    </button>
+                                    <br />
+                                    <div className="w-full text-center">
+                                        <a
+                                            href="/login"
+                                            className="transform text-lg font-bold text-sky-600 duration-300 hover:text-sky-800 mb-10"
+                                        >
+                                            Back to login
+                                        </a>
+                                    </div>
+                                </form>
+                            )}
+                        </Formik>
                     }
                 </section>
             </main>
--- src/UI/SatisfactoryPlanner.UI/tailwind.config.js
+++ src/UI/SatisfactoryPlanner.UI/tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}"
+    ],
     theme: {
-        extend: {},
+        extend: {}
     },
-    plugins: [],
+    plugins: []
 }
