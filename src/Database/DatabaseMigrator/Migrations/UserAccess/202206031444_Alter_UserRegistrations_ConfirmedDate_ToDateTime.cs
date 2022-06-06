735a7d9d8b4ff92a74fec99cc751f6f396178033
Bwaffles <thanaannis@gmail.com>
2022-06-06 11:20:00 -0400

    Made confirmed_date have a timestamp component because it didn't feel right that the register_date has it

---
+++ src/Database/DatabaseMigrator/Migrations/UserAccess/202206031444_Alter_UserRegistrations_ConfirmedDate_ToDateTime.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace DatabaseMigrator.Migrations.UserAccess
+{
+    [Migration(202206031444)]
+    public class Alter_UserRegistrations_ConfirmedDate_ToDateTime : Migration
+    {
+        public override void Down()
+        {
+            Alter.Column("confirmed_date")
+                .OnTable("user_registrations")
+                .InSchema("users")
+                .AsDate().Nullable();
+        }
+
+        public override void Up()
+        {
+            Alter.Column("confirmed_date")
+                .OnTable("user_registrations")
+                .InSchema("users")
+                .AsDateTime().Nullable();
+        }
+    }
+}
