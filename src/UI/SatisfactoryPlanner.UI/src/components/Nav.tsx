d6259a3329b844328b11810860c4787e01fb9d5d
Bwaffles <thanaannis@gmail.com>
2022-12-03 13:23:47 -0500

    feat: added a user dropdown in the nav to handle the logout and profile options for a cleaner nav look

--- src/UI/SatisfactoryPlanner.UI/src/components/Nav.tsx
+++ src/UI/SatisfactoryPlanner.UI/src/components/Nav.tsx
@@ -2,31 +2,41 @@ import * as React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import NavItem from "./NavItem";
+import UserDropdown from "./UserDropdown";
 
 const Nav = () => {
-    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+    const { isAuthenticated, loginWithRedirect } = useAuth0();
 
     return (
         <nav>
             <div className="sticky top-0 left-0 w-60 h-screen shadow-md bg-gray-800 px-3 flex flex-col justify-between">
                 <div className="mt-3">
-                    <NavItem to="/" text="Home" authenticated={false} />
-                    <NavItem to="/resources" text="Resources" authenticated={true} />
+                    <NavItem
+                        to="/"
+                        text="Home"
+                        authenticated={false}
+                    />
+                    <NavItem
+                        to="/resources"
+                        text="Resources"
+                        authenticated={true}
+                    />
                 </div>
                 <div className="mb-3">
-                    <NavItem to="/profile" text="Profile" authenticated={true} />
-                    <button
-                        className="w-full flex items-center py-4 px-6 overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:bg-sky-800"
-                        onClick={() => isAuthenticated
-                            ? logout({ returnTo: window.location.origin })
-                            : loginWithRedirect()}
-                    >
-                        {isAuthenticated ? "Log Out" : "Log In"}
-                    </button>
+                    {isAuthenticated ? (
+                        <UserDropdown />
+                    ) : (
+                        <button
+                            className="w-full flex items-center py-4 px-6 overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:bg-sky-800"
+                            onClick={() => loginWithRedirect()}
+                        >
+                            Log In
+                        </button>
+                    )}
                 </div>
             </div>
-        </nav >
+        </nav>
     );
-}
+};
 
 export default Nav;
---
+++ src/UI/SatisfactoryPlanner.UI/src/components/UserDropdown.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { useAuth0 } from "@auth0/auth0-react";
+import { NavLink } from "react-router-dom";
+
+const UserDropdown = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const { isAuthenticated, logout, user } = useAuth0();
+
+    function openMenu() {
+        setIsOpen(true);
+    }
+
+    function closeMenu() {
+        setIsOpen(false);
+    }
+
+    if (!isAuthenticated) return null;
+
+    // TODO split this into smaller components
+    return (
+        <div className="relative">
+            <div
+                className="flex items-center justify-between py-4 px-6 overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:bg-sky-800 cursor-pointer"
+                onClick={isOpen ? closeMenu : openMenu}
+            >
+                <div>
+                    <img
+                        className="w-7 h-7 mr-3 rounded-full inline"
+                        src={user?.picture}
+                        alt="profile pic"
+                    />
+                    {user?.name}
+                </div>
+            </div>
+            {isOpen && (
+                <div className="fixed bottom-0 left-60 w-48 py-3 px-4 flex flex-col justify-between bg-gray-700 rounded-tr border-t border-r border-gray-600">
+                    <div className="mb-4 text-white font-bold underline decoration-white">
+                        Settings
+                    </div>
+                    <div className="mb-3 pb-3 flex flex-col">
+                        <NavLink
+                            className="mb-4"
+                            to="/profile"
+                            onClick={closeMenu}
+                        >
+                            Profile
+                        </NavLink>
+                        <div
+                            className="flex justify-between items-center cursor-pointer"
+                            onClick={() =>
+                                logout({ returnTo: window.location.origin })
+                            }
+                        >
+                            Log Out
+                            <FontAwesomeIcon icon={faRightFromBracket} />
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default UserDropdown;
