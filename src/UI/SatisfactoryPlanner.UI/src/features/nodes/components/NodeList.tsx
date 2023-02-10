66c53517c98abd09c44d676156054d3bfc6e0543
Bwaffles <thanaannis@gmail.com>
2023-02-10 00:29:14 -0500

    feat: grouped the nodes by Biome

--- src/UI/SatisfactoryPlanner.UI/src/features/nodes/components/NodeList.tsx
+++ src/UI/SatisfactoryPlanner.UI/src/features/nodes/components/NodeList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { useGetNodes } from "../api/getNodes";
+import { Node } from "../types";
 
 type NodeListProps = {
     resourceId: string;
@@ -13,14 +14,32 @@ export const NodeList = ({ resourceId }: NodeListProps) => {
         return <span>Error: {(error as Error).message}</span>;
     }
 
+    const nodesByBiome = nodes!.reduce<Record<string, Node[]>>(
+        (group, node) => {
+            const { biome } = node;
+            group[biome] = group[biome] ?? [];
+            group[biome].push(node);
+
+            return group;
+        },
+        {}
+    );
+
     return (
         <>
             <h2 className="text-xl font-bold mb-6">Nodes</h2>
-            {nodes!.map((node) => {
+            {Object.keys(nodesByBiome).map((biome) => {
                 return (
-                    <div className="mb-4 py-6 px-10 bg-gray-800 rounded">
-                        {node.biome} - {node.id}
-                        <p>Purity: {node.purity}</p>
+                    <div className="mb-4 py-6 px-6 bg-gray-800 rounded">
+                        <h3 className="text-lg font-bold mb-4">{biome}</h3>
+                        {nodesByBiome[biome].map((node) => {
+                            return (
+                                <div className="mb-4 py-6 px-6 bg-gray-700 rounded">
+                                    {node.id}
+                                    <p>Purity: {node.purity}</p>
+                                </div>
+                            );
+                        })}
                     </div>
                 );
             })}
