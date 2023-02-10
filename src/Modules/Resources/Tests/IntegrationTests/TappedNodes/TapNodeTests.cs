8f3086677c50fbce6b1b524c58bdf3a2e98610ed
Bwaffles <thanaannis@gmail.com>
2023-02-09 23:32:32 -0500

    tests: retrieved a node to prepare the TapNode test

--- src/Modules/Resources/Tests/IntegrationTests/TappedNodes/TapNodeTests.cs
+++ src/Modules/Resources/Tests/IntegrationTests/TappedNodes/TapNodeTests.cs
@@ -1,4 +1,5 @@
-﻿using SatisfactoryPlanner.Modules.Resources.Application.Resources.GetResources;
+﻿using SatisfactoryPlanner.Modules.Resources.Application.Nodes.GetNodes;
+using SatisfactoryPlanner.Modules.Resources.Application.Resources.GetResources;
 using SatisfactoryPlanner.Modules.Resources.IntegrationTests.SeedWork;
 
 namespace SatisfactoryPlanner.Modules.Resources.IntegrationTests.TappedNodes
@@ -16,8 +17,17 @@ namespace SatisfactoryPlanner.Modules.Resources.IntegrationTests.TappedNodes
                 .Should()
                 .NotContain(resource => resource.ExtractedResources != 0);
 
+            var bauxite = resources.First(resource => resource.Name == "Bauxite");
+
+            var nodes = await ResourcesModule.ExecuteQueryAsync(new GetNodesQuery(worldId, bauxite.Id));
+            nodes
+                .Should()
+                .OnlyContain(node => node.ResourceId == bauxite.Id);
+
+            var node = nodes.First();
+
             // Tap node
-            
+
             // Check the extracted resource count again
         }
     }
