b5603a9fa58129fd054b2c78d14f9975b70862d2
Bwaffles <thanaannis@gmail.com>
2023-11-23 11:22:39 -0500

    refactor: rename old pioneers folder to worlds

---
+++ src/Modules/Worlds/Tests/ArchTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SatisfactoryPlanner.Modules.Worlds.ArchTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
