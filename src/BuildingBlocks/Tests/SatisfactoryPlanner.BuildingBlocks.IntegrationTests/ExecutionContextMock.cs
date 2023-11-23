d780ed2e82657be6607ef082dc23d7e836b75427
Bwaffles <thanaannis@gmail.com>
2023-11-23 11:01:34 -0500

    refactor: moved ExecutionContextMock to building blocks

---
+++ src/BuildingBlocks/Tests/SatisfactoryPlanner.BuildingBlocks.IntegrationTests/ExecutionContextMock.cs
@@ -0,0 +1,13 @@
+﻿using SatisfactoryPlanner.BuildingBlocks.Application;
+
+namespace SatisfactoryPlanner.BuildingBlocks.IntegrationTests
+{
+    public class ExecutionContextMock(Guid userId) : IExecutionContextAccessor
+    {
+        public Guid UserId { get; set; } = userId;
+
+        public Guid CorrelationId => throw new InvalidOperationException();
+
+        public bool IsAvailable => throw new InvalidOperationException();
+    }
+}
