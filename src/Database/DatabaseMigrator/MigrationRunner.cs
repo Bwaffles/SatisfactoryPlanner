3466d1809684b8a0a09af4ff5a1f4732e7a4b67e
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2024-03-20 11:21:41 -0400

    refactor: extracted logic to perform migrations to be reusable

---
+++ src/Database/DatabaseMigrator/MigrationRunner.cs
@@ -0,0 +1,51 @@
+﻿using DatabaseMigrator.Migrations;
+using DatabaseMigrator.Migrations.Factories;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DatabaseMigrator
+{
+    public class MigrationRunner
+    {
+        private readonly IMigrationRunner _migrationRunner;
+
+        public MigrationRunner(string serverConnectionString, string connectionString)
+        {
+            Database.EnsureDatabase(serverConnectionString, "satisfactory-planner");
+
+            var servicesProvider = new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddPostgres()
+                    .WithGlobalConnectionString(connectionString)
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(Create_Factories).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                //.AddScoped<PostgresQuoter, NoQuoteQuoter>()
+                // Build the service provider
+                .BuildServiceProvider(false);
+
+            _migrationRunner = servicesProvider.GetRequiredService<IMigrationRunner>();
+        }
+
+        public void ListMigrations() => _migrationRunner.ListMigrations();
+
+        public void Migrate()
+        {
+            _migrationRunner.MigrateUp();
+        }
+
+        public void RerunPreviousMigration()
+        {
+            _migrationRunner.Rollback(1);
+            _migrationRunner.ListMigrations();
+            _migrationRunner.MigrateUp();
+        }
+
+        public void Rollback()
+        {
+            _migrationRunner.Rollback(1);
+        }
+    }
+}
