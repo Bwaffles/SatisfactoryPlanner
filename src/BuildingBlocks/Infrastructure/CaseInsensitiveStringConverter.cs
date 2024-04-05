92ba304949a5ca61bd3df78a145c1a6ef84f05f2
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2024-04-05 16:19:35 -0400

    feat: add SetUpProductionLineCommand

---
+++ src/BuildingBlocks/Infrastructure/CaseInsensitiveStringConverter.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using SatisfactoryPlanner.BuildingBlocks.Domain;
+
+namespace SatisfactoryPlanner.BuildingBlocks.Infrastructure
+{
+    public class CaseInsensitiveStringConverter : ValueConverter<CaseInsensitiveString, string>
+    {
+        public CaseInsensitiveStringConverter()
+            : base(caseInsensitiveString => caseInsensitiveString.Value, value => new CaseInsensitiveString(value)) { }
+    }
+}
