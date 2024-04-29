4aba1bc60b806b9ace9d22faa05d00e97d885a8f
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2024-04-29 13:21:31 -0400

    refactor: move all of Startup to Program

--- src/API/SatisfactoryPlanner.API/Program.cs
+++ src/API/SatisfactoryPlanner.API/Program.cs
@@ -1,8 +1,29 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using SatisfactoryPlanner.API;
+using Hellang.Middleware.ProblemDetails;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using SatisfactoryPlanner.API.Configuration.Authorization.Permissions;
+using SatisfactoryPlanner.API.Configuration.Authorization.Worlds;
+using SatisfactoryPlanner.API.Configuration.ExecutionContext;
+using SatisfactoryPlanner.API.Configuration.Extensions;
+using SatisfactoryPlanner.API.Configuration.Routing;
+using SatisfactoryPlanner.API.Configuration.Validation;
+using SatisfactoryPlanner.API.Modules.Production;
+using SatisfactoryPlanner.API.Modules.Resources;
+using SatisfactoryPlanner.API.Modules.UserAccess;
+using SatisfactoryPlanner.API.Modules.Worlds;
+using SatisfactoryPlanner.BuildingBlocks.Application;
+using SatisfactoryPlanner.BuildingBlocks.Domain;
+using SatisfactoryPlanner.Modules.Production.Infrastructure.Configuration;
+using SatisfactoryPlanner.Modules.Resources.Infrastructure.Configuration;
+using SatisfactoryPlanner.Modules.UserAccess.Infrastructure.Configuration;
+using SatisfactoryPlanner.Modules.Worlds.Infrastructure.Configuration;
 using Serilog;
 using Serilog.Formatting.Compact;
+using ILogger = Serilog.ILogger;
 
 var _logger = new LoggerConfiguration()
     .Enrich.FromLogContext()
@@ -20,18 +41,146 @@ _loggerForApi.Information("Application started.");
 
 var builder = WebApplication.CreateBuilder(args);
 
-var startup = new Startup(builder.Configuration);
-
-startup.ConfigureServices(builder.Services);
+ConfigureServices(builder);
 
 // Using a custom DI container.
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-builder.Host.ConfigureContainer<ContainerBuilder>(startup.ConfigureContainer);
+builder.Host.ConfigureContainer<ContainerBuilder>(RegisterModules);
 
 var app = builder.Build();
 
-startup.Configure(app, app.Environment, _logger);
+Configure(app, app.Environment, _logger, builder.Configuration);
 
 app.MapControllers();
 
 app.Run();
+
+static void ConfigureServices(WebApplicationBuilder builder)
+{
+    ConfigureAuthenticationService(builder);
+    ConfigureAuthorizationService(builder);
+
+    builder.Services.AddControllers(options =>
+    {
+        options.Filters.Add(new ProducesAttribute("application/json"));
+        options.Conventions.Add(new RouteTokenTransformerConvention(new SlugifyParameterTransformer()));
+    });
+
+    builder.Services.AddRouting(options =>
+    {
+        options.LowercaseUrls = true;
+    });
+
+    builder.Services.AddSwaggerDocumentation();
+
+    builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+    builder.Services.AddSingleton<IExecutionContextAccessor, ExecutionContextAccessor>();
+
+    builder.Services.AddProblemDetails(options =>
+    {
+        options.Map<InvalidCommandException>(ex => new InvalidCommandProblemDetails(ex));
+        options.Map<BusinessRuleValidationException>(
+            ex => new BusinessRuleValidationExceptionProblemDetails(ex));
+    });
+}
+
+static void ConfigureAuthenticationService(WebApplicationBuilder builder) => builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
+        options.Audience = builder.Configuration["Auth0:Audience"];
+    });
+
+static void ConfigureAuthorizationService(WebApplicationBuilder builder)
+{
+    builder.Services.AddAuthorization(options =>
+    {
+        options.AddPolicy(HasPermissionAttribute.HasPermissionPolicyName, policyBuilder =>
+        {
+            policyBuilder.Requirements.Add(new HasPermissionAuthorizationRequirement());
+        });
+        options.AddPolicy(WorldAuthorizationAttribute.HasPermissionPolicyName, policyBuilder =>
+        {
+            policyBuilder.Requirements.Add(new WorldAuthorizationRequirement());
+        });
+    });
+
+    builder.Services.AddScoped<IAuthorizationHandler, HasPermissionAuthorizationHandler>();
+    builder.Services.AddScoped<IAuthorizationHandler, WorldAuthorizationHandler>();
+}
+
+static void RegisterModules(ContainerBuilder containerBuilder)
+{
+    containerBuilder.RegisterModule(new WorldsAutofacModule());
+    containerBuilder.RegisterModule(new ResourcesAutofacModule());
+    containerBuilder.RegisterModule(new ProductionAutofacModule());
+    containerBuilder.RegisterModule(new UserAccessAutofacModule());
+}
+
+static void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger logger, ConfigurationManager configuration)
+{
+    app.UseCors(builder =>
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+    );
+
+    InitializeModules(app, logger, configuration);
+
+    app.UseMiddleware<CorrelationMiddleware>();
+
+    app.UseProblemDetails();
+
+    if (env.IsDevelopment())
+    {
+        app.UseSwaggerDocumentation();
+    }
+    // else
+    // {
+    //     app.UseHsts();
+    // }
+
+    //app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+
+    // To be used by WorldAuthorization to get world id from the body of the request
+    app.Use((context, next) =>
+    {
+        context.Request.EnableBuffering(1_000_000);
+        return next();
+    });
+    app.UseAuthorization();
+}
+
+static void InitializeModules(IApplicationBuilder app, ILogger logger, ConfigurationManager configuration)
+{
+    var container = app.ApplicationServices.GetAutofacRoot();
+    var executionContextAccessor = container.Resolve<IExecutionContextAccessor>();
+    var connectionString = configuration.GetConnectionString("SatisfactoryPlanner") ?? throw new InvalidOperationException("SatisfactoryPlanner connection string not defined.");
+
+    ProductionStartup.Initialize(
+        connectionString,
+        executionContextAccessor,
+        logger
+    );
+
+    WorldsStartup.Initialize(
+        connectionString,
+        executionContextAccessor,
+        logger
+    );
+
+    ResourcesStartup.Initialize(
+        connectionString,
+        executionContextAccessor,
+        logger
+    );
+
+    UserAccessStartup.Initialize(
+        connectionString,
+        executionContextAccessor,
+        logger);
+}
