303e882fdd90b4d1b2b8825bb20cc17725000ee0
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2024-05-02 12:24:23 -0400

    refactor: convert DowngradeExtractor to endpoint

--- src/API/SatisfactoryPlanner.API/EndPoints/Resources/WorldNodes/WorldNodesController.cs
+++ src/API/SatisfactoryPlanner.API/EndPoints/Resources/WorldNodes/WorldNodesController.cs
@@ -4,7 +4,6 @@ using SatisfactoryPlanner.API.Configuration.Authorization.Permissions;
 using SatisfactoryPlanner.API.Configuration.Authorization.Worlds;
 using SatisfactoryPlanner.Modules.Resources.Application.Contracts;
 using SatisfactoryPlanner.Modules.Resources.Application.WorldNodes.DismantleExtractor;
-using SatisfactoryPlanner.Modules.Resources.Application.WorldNodes.DowngradeExtractor;
 
 namespace SatisfactoryPlanner.API.Modules.Resources.WorldNodes
 {
@@ -12,27 +11,6 @@ namespace SatisfactoryPlanner.API.Modules.Resources.WorldNodes
     [Route("api")]
     public class WorldNodesController(IResourcesModule module) : Controller
     {
-        /// <summary>
-        ///     Downgrade the extractor used to extract resources from the world node.
-        /// </summary>
-        [Authorize]
-        [HasPermission(ResourcesPermissions.DowngradeExtractor)]
-        [WorldAuthorization]
-        [HttpPost("worlds/{worldId}/nodes/{nodeId}/downgrade-extractor")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<IActionResult> DowngradeExtractor([FromRoute] Guid worldId,
-            [FromRoute] Guid nodeId,
-            [FromBody] DowngradeExtractorRequest request)
-        {
-            await module.ExecuteCommandAsync(new DowngradeExtractorCommand(
-                worldId,
-                nodeId,
-                request.ExtractorId
-            ));
-
-            return NoContent();
-        }
-
         /// <summary>
         ///     Dismantle the extractor used to extract resources from the world node.
         /// </summary>
