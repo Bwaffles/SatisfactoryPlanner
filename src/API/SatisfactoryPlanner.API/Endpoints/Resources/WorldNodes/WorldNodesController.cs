eeae5513554e4a76b547fbcf0c82722add21c3de
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2024-05-02 11:49:50 -0400

    refactor: convert IncreaseWorldNodeExtractionRate to endpoint

--- src/API/SatisfactoryPlanner.API/Endpoints/Resources/WorldNodes/WorldNodesController.cs
+++ src/API/SatisfactoryPlanner.API/Endpoints/Resources/WorldNodes/WorldNodesController.cs
@@ -6,8 +6,6 @@ using SatisfactoryPlanner.Modules.Resources.Application.Contracts;
 using SatisfactoryPlanner.Modules.Resources.Application.WorldNodes.DecreaseExtractionRate;
 using SatisfactoryPlanner.Modules.Resources.Application.WorldNodes.DismantleExtractor;
 using SatisfactoryPlanner.Modules.Resources.Application.WorldNodes.DowngradeExtractor;
-using SatisfactoryPlanner.Modules.Resources.Application.WorldNodes.IncreaseExtractionRate;
-using SatisfactoryPlanner.Modules.Resources.Application.WorldNodes.TapWorldNode;
 using SatisfactoryPlanner.Modules.Resources.Application.WorldNodes.UpgradeExtractor;
 
 namespace SatisfactoryPlanner.API.Modules.Resources.WorldNodes
@@ -16,27 +14,6 @@ namespace SatisfactoryPlanner.API.Modules.Resources.WorldNodes
     [Route("api")]
     public class WorldNodesController(IResourcesModule module) : Controller
     {
-        /// <summary>
-        ///     Increase the extraction rate of resources from the world node.
-        /// </summary>
-        [Authorize]
-        [HasPermission(ResourcesPermissions.IncreaseWorldNodeExtractionRate)]
-        [WorldAuthorization]
-        [HttpPost("worlds/{worldId}/nodes/{nodeId}/increase-extraction-rate")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<IActionResult> IncreaseWorldNodeExtractionRate([FromRoute] Guid worldId,
-            [FromRoute] Guid nodeId,
-            [FromBody] IncreaseWorldNodeExtractionRateRequest request)
-        {
-            await module.ExecuteCommandAsync(new IncreaseExtractionRateCommand(
-                worldId,
-                nodeId,
-                request.ExtractionRate
-            ));
-
-            return NoContent();
-        }
-
         /// <summary>
         ///     Decrease the extraction rate of resources from the world node.
         /// </summary>
