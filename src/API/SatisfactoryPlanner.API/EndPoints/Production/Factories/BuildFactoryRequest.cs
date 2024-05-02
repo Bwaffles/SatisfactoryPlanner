aaaebd9fc9605268dfb485cb9c2630d912c65428
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2024-05-02 12:34:59 -0400

    refactor: remove Factories controller since it's not used right now and unsecured

--- src/API/SatisfactoryPlanner.API/EndPoints/Production/Factories/BuildFactoryRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SatisfactoryPlanner.API.Modules.Production.Factories
-{
-    public class BuildFactoryRequest
-    {
-        public string Name { get; set; }
-    }
-}
--- src/API/SatisfactoryPlanner.API/EndPoints/Production/Factories/BuildSubFactoryRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SatisfactoryPlanner.API.Modules.Production.Factories
-{
-    public class BuildSubFactoryRequest
-    {
-        public string Name { get; set; }
-    }
-}
