76ea06fbcc18b9e78f0523bb03dc9b47f7649d98
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2024-05-02 10:36:40 -0400

    refactor: convert GetCurrentPioneerWorlds to endpoint

--- src/API/SatisfactoryPlanner.API/Endpoints/Worlds/Worlds/GetCurrentPioneerWorlds.cs
+++ src/API/SatisfactoryPlanner.API/Endpoints/Worlds/Worlds/GetCurrentPioneerWorlds.cs
@@ -3,24 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 using SatisfactoryPlanner.API.Configuration.Authorization;
 using SatisfactoryPlanner.Modules.Worlds.Application.Contracts;
 using SatisfactoryPlanner.Modules.Worlds.Application.Worlds.GetCurrentPioneerWorlds;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace SatisfactoryPlanner.API.Modules.Worlds.Worlds
 {
-    [Route("api/worlds/[controller]")]
     [ApiController]
-    public class WorldsController(IWorldsModule module) : ControllerBase
+    public class GetCurrentPioneerWorlds(IWorldsModule module) : ControllerBase
     {
-        /// <summary>
-        ///     Get the worlds for the currently logged in pioneer.
-        /// </summary>
-        /// <response code="200">
-        ///     Returns their worlds as a list of <see cref="PioneerWorldDto" />.
-        /// </response>
         [Authorize]
         [NoPermissionRequired]
-        [HttpGet("@me")]
-        [ProducesResponseType(typeof(List<PioneerWorldDto>), StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetCurrentPioneerWorlds()
+        [HttpGet("api/worlds/worlds/@me")]
+        [SwaggerOperation(
+            Summary = "Get the worlds for the current pioneer.",
+            Description = "Gets the worlds for the currently authenticated user based on the user id in the access token.",
+            Tags = ["Worlds"])]
+        [SwaggerResponse(200, Type = typeof(List<PioneerWorldDto>))]
+        public async Task<IActionResult> HandleAsync()
         {
             var worlds = await module.ExecuteQueryAsync(new GetCurrentPioneerWorldsQuery());
             return Ok(worlds);
