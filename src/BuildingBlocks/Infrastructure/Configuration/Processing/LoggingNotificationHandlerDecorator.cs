b904642081af16a3efec0a95530084b8d41bb002
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2024-05-30 15:30:28 -0400

    fix: improve logging by adding some context
    Some notifications had no context. This isn't perfect, but better than seeing [] at least.

--- src/BuildingBlocks/Infrastructure/Configuration/Processing/LoggingNotificationHandlerDecorator.cs
+++ src/BuildingBlocks/Infrastructure/Configuration/Processing/LoggingNotificationHandlerDecorator.cs
@@ -1,6 +1,11 @@
 ﻿using MediatR;
 using SatisfactoryPlanner.BuildingBlocks.Application.Events;
+using SatisfactoryPlanner.BuildingBlocks.Domain;
+using SatisfactoryPlanner.BuildingBlocks.Infrastructure.EventBus;
 using Serilog;
+using Serilog.Context;
+using Serilog.Core;
+using Serilog.Events;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,30 +17,51 @@ namespace SatisfactoryPlanner.BuildingBlocks.Infrastructure.Configuration.Proces
     {
         public async Task Handle(T notification, CancellationToken cancellationToken)
         {
-            var notificationName = notification.GetType().Name;
-
-            try
+            using (LogContext.Push(new NotificationLogEnricher(notification)))
             {
-                if (notification is IDomainEventNotification)
-                    logger.Information("Sending {Notification}", notificationName);
-                else
-                    logger.Information("Publishing {Event}", notificationName);
+                var notificationName = notification.GetType().Name;
+                try
+                {
+                    if (notification is IDomainEventNotification)
+                        logger.Information("Sending {Notification}", notificationName);
+                    else
+                        logger.Information("Publishing {Event}", notificationName);
 
-                await decorated.Handle(notification, cancellationToken);
+                    await decorated.Handle(notification, cancellationToken);
 
-                if (notification is IDomainEventNotification)
-                    logger.Information("Successfully sent {Notification}", notificationName);
-                else
-                    logger.Information("Published {Event}", notificationName);
+                    if (notification is IDomainEventNotification)
+                        logger.Information("Successfully sent {Notification}", notificationName);
+                    else
+                        logger.Information("Published {Event}", notificationName);
+                }
+                catch (Exception exception)
+                {
+                    if (notification is IDomainEventNotification)
+                        logger.Error(exception, "Sending {Notification} failed", notificationName);
+                    else
+                        logger.Error(exception, "Publishing {Event} failed", notificationName);
+
+                    throw;
+                }
             }
-            catch (Exception exception)
-            {
-                if (notification is IDomainEventNotification)
-                    logger.Error(exception, "Sending {Notification} failed", notificationName);
-                else
-                    logger.Error(exception, "Publishing {Event} failed", notificationName);
+        }
 
-                throw;
+        private class NotificationLogEnricher(INotification notification) : ILogEventEnricher
+        {
+            public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+            {
+                if (notification is IDomainEventNotification domainEventNotification)
+                {
+                    logEvent.AddOrUpdateProperty(new LogEventProperty("Context", new ScalarValue($"Notification:{domainEventNotification.Id}")));
+                }
+                else if (notification is IntegrationEvent integrationEvent)
+                {
+                    logEvent.AddOrUpdateProperty(new LogEventProperty("Context", new ScalarValue($"IntegrationEvent:{integrationEvent.Id}")));
+                }
+                else if (notification is DomainEventBase domainEventBase)
+                {
+                    logEvent.AddOrUpdateProperty(new LogEventProperty("Context", new ScalarValue($"DomainEvent:{domainEventBase.Id}")));
+                }
             }
         }
     }
