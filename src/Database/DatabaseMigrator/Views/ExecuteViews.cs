b6190963eee0a2998152bafe669fa89c25781e91
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2024-06-06 09:06:42 -0400

    feat: add logic to create views in database migrations

---
+++ src/Database/DatabaseMigrator/Views/ExecuteViews.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+using System.Linq;
+using System.Reflection;
+
+namespace DatabaseMigrator.Views
+{
+    /// <summary>
+    /// A migration that will run after migrations have been applied to run all of the views.
+    /// To add a new view, create a new sql file anywhere in this Views directory to create or replace the view.
+    /// </summary>
+    [Maintenance(MigrationStage.AfterAll)]
+    public class ExecuteViews : Migration
+    {
+        public override void Down()
+        {
+            // No-op
+        }
+
+        public override void Up()
+        {
+            var views = Assembly
+                .GetExecutingAssembly()
+                .GetManifestResourceNames()
+                .Where(_ => _.StartsWith("Views/"));
+
+            foreach (var view in views)
+                Execute.EmbeddedScript(view);
+        }
+    }
+}
