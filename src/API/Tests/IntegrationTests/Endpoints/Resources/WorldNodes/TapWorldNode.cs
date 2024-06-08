7bcddee92a0450bb77bf379c023ca0dbb307b03f
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2024-06-08 11:48:39 -0400

    feat: add test for TapWorldNode endpoint

---
+++ src/API/Tests/IntegrationTests/Endpoints/Resources/WorldNodes/TapWorldNode.cs
@@ -0,0 +1,35 @@
+﻿using SatisfactoryPlanner.API.IntegrationTests.Endpoints.Resources.WorldNodes;
+using SatisfactoryPlanner.API.IntegrationTests.Endpoints.Worlds;
+using SatisfactoryPlanner.API.Modules.Resources.WorldNodes;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace SatisfactoryPlanner.API.IntegrationTests.Endpoints.UserAccess.Users
+{
+    public static class TapWorldNode
+    {
+        public static async Task<HttpResponseMessage> Execute(HttpClient client, Guid worldId, Guid nodeId, Guid extractorId)
+            => await client.PostAsJsonAsync($"api/worlds/{worldId}/nodes/{nodeId}/tap", new TapWorldNodeRequest
+            {
+                ExtractorId = extractorId
+            });
+
+        [TestFixture]
+        public class Tests : IntegrationTest
+        {
+            [Test]
+            public async Task HappyPath()
+            {
+                var worldId = await new WorldFixture(Client).CreateEmptyWorld();
+                var ironOreNode = await new WorldNodeFixture(Client)
+                    .FindWorldNode(worldId, worldNode => worldNode.ResourceName == "Iron Ore");
+                var ironOreNodeDetailsResult = await GetWorldNodeDetails.GetResult(Client, worldId, ironOreNode.Id);
+                var extractor = ironOreNodeDetailsResult.Details.AvailableExtractors.First();
+
+                var response = await Execute(Client, worldId, ironOreNode.Id, extractor.Id);
+
+                response.Should().HaveStatusCode(HttpStatusCode.OK);
+            }
+        }
+    }
+}
