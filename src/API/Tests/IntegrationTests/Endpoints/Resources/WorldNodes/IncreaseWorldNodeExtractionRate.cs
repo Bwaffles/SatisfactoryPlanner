8bf820afb3ae87db99a4e8f556edd007508fd5c5
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2024-06-26 21:59:09 -0400

    feat: add api test for IncreaseExtractionRate end point

---
+++ src/API/Tests/IntegrationTests/Endpoints/Resources/WorldNodes/IncreaseWorldNodeExtractionRate.cs
@@ -0,0 +1,28 @@
+﻿using SatisfactoryPlanner.API.Modules.Resources.WorldNodes;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace SatisfactoryPlanner.API.IntegrationTests.Endpoints.Resources.WorldNodes;
+
+public static class IncreaseWorldNodeExtractionRate
+{
+    public static async Task<HttpResponseMessage> Execute(HttpClient client, Guid worldId, Guid nodeId, decimal extractionRate)
+        => await client.PostAsJsonAsync($"api/worlds/{worldId}/nodes/{nodeId}/increase-extraction-rate", new IncreaseWorldNodeExtractionRateRequest
+        {
+            ExtractionRate = extractionRate
+        });
+
+    [TestFixture]
+    public class Tests : IntegrationTest
+    {
+        [Test]
+        public async Task HappyPath()
+        {
+            var (worldId, nodeId) = await new TappedWorldNodeFixture(Client).Create();
+
+            var response = await Execute(Client, worldId, nodeId, 10);
+
+            response.Should().HaveStatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
