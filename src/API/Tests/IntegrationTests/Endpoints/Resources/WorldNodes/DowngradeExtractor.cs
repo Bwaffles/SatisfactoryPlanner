8ba7e73105520f8308917f818354eb82616e7486
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2024-06-28 11:46:03 -0400

    test: add api test for downgrade extractor end point

---
+++ src/API/Tests/IntegrationTests/Endpoints/Resources/WorldNodes/DowngradeExtractor.cs
@@ -0,0 +1,30 @@
+﻿using SatisfactoryPlanner.API.Modules.Resources.WorldNodes;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace SatisfactoryPlanner.API.IntegrationTests.Endpoints.Resources.WorldNodes;
+
+public static class DowngradeExtractor
+{
+    public static async Task<HttpResponseMessage> Execute(HttpClient client, Guid worldId, Guid nodeId, Guid extractorId)
+        => await client.PostAsJsonAsync($"api/worlds/{worldId}/nodes/{nodeId}/downgrade-extractor", new UpgradeExtractorRequest
+        {
+            ExtractorId = extractorId
+        });
+
+    [TestFixture]
+    public class Tests : IntegrationTest
+    {
+        [Test]
+        public async Task HappyPath()
+        {
+            var settings = await new TappedWorldNodeFixture(Client)
+                .Create("Iron Ore", "Miner Mk.3");
+
+            var fasterExtractor = settings.AvailableExtractors.First(extractor => extractor.MaxExtractionRate < settings.CurrentExtractor.MaxExtractionRate);
+            var response = await Execute(Client, settings.WorldId, settings.NodeId, fasterExtractor.Id);
+
+            response.Should().HaveStatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
