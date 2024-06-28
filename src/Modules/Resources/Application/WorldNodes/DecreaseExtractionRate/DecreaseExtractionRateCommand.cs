325da92246ced10ec14c37f227f0cc4da03ae984
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2024-06-27 22:46:00 -0400

    feat: add api test for decrease extraction rate end point

--- src/Modules/Resources/Application/WorldNodes/DecreaseExtractionRate/DecreaseExtractionRateCommand.cs
+++ src/Modules/Resources/Application/WorldNodes/DecreaseExtractionRate/DecreaseExtractionRateCommand.cs
@@ -1,21 +1,37 @@
-﻿using SatisfactoryPlanner.Modules.Resources.Application.Contracts;
+﻿using MediatR;
+using SatisfactoryPlanner.BuildingBlocks.Application;
+using SatisfactoryPlanner.Modules.Resources.Application.Configuration.Commands;
+using SatisfactoryPlanner.Modules.Resources.Application.Contracts;
+using SatisfactoryPlanner.Modules.Resources.Domain.Nodes;
+using SatisfactoryPlanner.Modules.Resources.Domain.WorldNodes;
+using SatisfactoryPlanner.Modules.Resources.Domain.Worlds;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace SatisfactoryPlanner.Modules.Resources.Application.WorldNodes.DecreaseExtractionRate
+namespace SatisfactoryPlanner.Modules.Resources.Application.WorldNodes.DecreaseExtractionRate;
+
+public class DecreaseExtractionRateCommand(Guid worldId, Guid nodeId, decimal extractionRate) : CommandBase
 {
-    public class DecreaseExtractionRateCommand : CommandBase
+    public Guid WorldId { get; } = worldId;
+    public Guid NodeId { get; } = nodeId;
+    public decimal ExtractionRate { get; } = extractionRate;
+}
+
+internal class DecreaseExtractionRateCommandHandler(IWorldNodeRepository worldNodeRepository) : ICommandHandler<DecreaseExtractionRateCommand>
+{
+    private readonly IWorldNodeRepository _worldNodeRepository = worldNodeRepository;
+
+    public async Task<Unit> Handle(DecreaseExtractionRateCommand command, CancellationToken cancellationToken)
     {
-        public Guid WorldId { get; }
+        var worldNode = await _worldNodeRepository.FindAsync(
+            new WorldId(command.WorldId),
+            new NodeId(command.NodeId));
+        if (worldNode == null)
+            throw new InvalidCommandException("World node must exist.");
 
-        public Guid NodeId { get; }
+        worldNode.DecreaseExtractionRate(ExtractionRate.Of(command.ExtractionRate));
 
-        public decimal ExtractionRate { get; }
-
-        public DecreaseExtractionRateCommand(Guid worldId, Guid nodeId, decimal extractionRate)
-        {
-            WorldId = worldId;
-            NodeId = nodeId;
-            ExtractionRate = extractionRate;
-        }
+        return Unit.Value;
     }
 }
