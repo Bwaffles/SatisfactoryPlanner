494c0eb478888fa614dfcd40d3b59b7139e88766
Bwaffles <5383859+Bwaffles@users.noreply.github.com>
2025-07-10 11:10:09 -0400

    tests: add api test for DismantleExtractor end point

---
+++ src/API/Tests/IntegrationTests/Endpoints/Resources/WorldNodes/DismantleExtractor.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+
+namespace SatisfactoryPlanner.API.IntegrationTests.Endpoints.Resources.WorldNodes;
+
+public static class DismantleExtractor
+{
+    public static async Task<HttpResponseMessage> Execute(HttpClient client, Guid worldId, Guid nodeId)
+        => await client.PostAsJsonAsync($"api/worlds/{worldId}/nodes/{nodeId}/dismantle-extractor", new { });
+
+    [TestFixture]
+    public class Tests : IntegrationTest
+    {
+        [Test]
+        public async Task HappyPath()
+        {
+            var (worldId, nodeId) = await new TappedWorldNodeFixture(Client).Create();
+
+            var response = await Execute(Client, worldId, nodeId);
+
+            response.Should().HaveStatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
